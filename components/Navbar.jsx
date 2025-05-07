bfa224e30299120f08932d304bc7d0feae8c70eb
Mladen Savic <mladen.savic@pricespider.com>
2025-05-07 20:18:39 +0200

    feat: add homepage components and footer

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -50,10 +50,10 @@ const Navbar = () => {
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             {/* <!-- Logo --> */}
             <Link className="flex flex-shrink-0 items-center" href="/">
-              <Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
+              <Image className="h-10 w-auto" src={logo} alt="Serbia Rentals" />
 
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                PropertyPulse
+                Serbia Rentals
               </span>
             </Link>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
